1bb328a5b799bbf59492bf65cacffa3ac5af3133
Vanessa Sierra <sierrava212@yahoo.com>
2025-11-21 14:38:16 -0500

    completed product class

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,4 +1,4 @@
-export class Product {
+export class Product{
   // Properties
 id: number;
 title: string;
@@ -18,5 +18,15 @@ constructor (id:number, title:string, description:string ,category:string, price
     this.price = price;
     this.discountPercentage = discountPercentage;
 }
+ //*PRODUCT DETAILS BELOW// METHODS STATED BELOW:
 
+    displayDetails():string {
+        return `id: ${this.id}, Title: ${this.title}, Description: ${this.description}, Category: ${this.category}, Price: ${this.price},
+        DiscountPercentage: ${this.discountPercentage}`;
+    }
+
+    getPriceWithDiscount():number {
+        return this.price * (this.discountPercentage / 100) + this.price
+
+    }
 }
