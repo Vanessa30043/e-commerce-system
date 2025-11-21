5e6442d230cfcb0a05e4321ebc5f5fafc1b4f73e
Vanessa Sierra <sierrava212@yahoo.com>
2025-11-21 11:53:10 -0500

    created  Product class and constructor

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,4 +1,4 @@
-class Product {
+export class Product {
   // Properties
 id: number;
 title: string;
@@ -7,3 +7,16 @@ category: string;
 price: number;
 discountPercentage: number;
 
+
+// A constructor : initalizes  the variables above 
+
+constructor (id:number, title:string, description:string ,category:string, price: number,discountPercentage: number){
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.category = category;
+    this.price = price;
+    this.discountPercentage = discountPercentage;
+}
+
+}
