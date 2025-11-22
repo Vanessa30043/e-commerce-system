0ce6938725124f2748e81e32823d58cb8edea7d1
Vanessa Sierra <sierrava212@yahoo.com>
2025-11-21 23:24:04 -0500

    added APIservices

---
+++ src/services/apiService.ts
@@ -0,0 +1,21 @@
+export const fetchProducts = async (): Promise<{
+  products: {
+    id: number;
+    title: string;
+    description: string;
+    category: string;
+    price: number;
+    discountPercentage: number;
+  }[];
+}> => {
+  try {
+    const response = await fetch("https://dummyjson.com/products");
+    const data = await response.json();
+    return data; // now typed without using an interface
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  } finally {
+    console.log("Request finished (success or error)");
+  }
+};
